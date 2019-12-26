e42c12ac2c5d1f1b3436c865446381128de66c30
Max Hübner <maxshuebner@gmail.com>
2019-12-27 00:03:29 +0100

    clean up scripts

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,15 +1,11 @@
+### Create 'Send to > SeeSharp' shortcut
+
 $buildPath = 'C:\SeeSharp\win10\x64'
-$documentsPath = [Environment]::GetFolderPath("MyDocuments")
-$pagesPath = Join-Path $documentsPath -ChildPath SeeSharp | Join-Path -ChildPath pages
-Write-Host $pagesPath -ForegroundColor Green
 
-
-#exit
-### build App
 Write-Host "================= BUILDING APP =================" -ForegroundColor Green
 
-cd SeeSharp.Desktop
+cd ../SeeSharp.Desktop
 dotnet publish -o $buildPath -c RELEASE -f netcoreapp3.0 -r win10-x64 /p:PublishSingleFile=true /p:IncludeSymbolsInSingleFile=true
-cd ..
+cd ../scripts
 
 Write-Host "================== ALL DONE! ===================" -ForegroundColor Green
--- scripts/createSendToShortcut.ps1
+++ scripts/createSendToShortcut.ps1
@@ -1,7 +1,11 @@
 ### Create 'Send to > SeeSharp' shortcut
-Write-Host "==== CREATING 'Send to > SeeSharp' SHORTCUT ====" -ForegroundColor Green
-$wshshell = New-Object -com wscript.shell
 
+$documentsPath = [Environment]::GetFolderPath("MyDocuments")
+$pagesPath = Join-Path $documentsPath -ChildPath SeeSharp | Join-Path -ChildPath pages
+
+Write-Host "==== CREATING 'Send to > SeeSharp' SHORTCUT ====" -ForegroundColor Green
+
+$wshshell = New-Object -com wscript.shell
 $shortcutPath = Join-Path -Path $wshshell.SpecialFolders.Item('sendto') -ChildPath 'SeeSharp.lnk'
 if((Test-Path $shortcutPath)){
     Write-Host "    shortcut already set" -ForegroundColor Green
@@ -11,5 +15,6 @@ else{
     $shortcut.TargetPath = $pagesPath
     $shortcut.Description = 'this shortcut was created by SeeSharp'
     $shortcut.Save()
-    Write-Host "    shortcut created" -ForegroundColor Green
+    Write-Host "    shortcut created at $pagesPath" -ForegroundColor Green
 }
+Write-Host "================== ALL DONE! ===================" -ForegroundColor Green
