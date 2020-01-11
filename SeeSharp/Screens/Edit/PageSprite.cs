d731f31a86d78ca4e83820bf952a4b9496028f60
Max Hübner <maxshuebner@gmail.com>
2020-01-11 12:55:37 +0100

    read AspectRatio directly from Texture

--- SeeSharp/Screens/Edit/PageSprite.cs
+++ SeeSharp/Screens/Edit/PageSprite.cs
@@ -1,10 +1,8 @@
-using System.IO;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using SeeSharp.Models;
-using SixLabors.ImageSharp;
 
 namespace SeeSharp.Screens.Edit
 {
@@ -27,9 +25,7 @@ namespace SeeSharp.Screens.Edit
 
             if (FillMode == FillMode.Fit)
             {
-                //read aspect ratio from file
-                var image = Image.Load(Path.Combine(storage.GetStorageForDirectory("pages").GetFullPath(string.Empty), _page.Value.Name));
-                FillAspectRatio = (float) image.Width / (float) image.Height;
+                FillAspectRatio = (float) Texture.Width / (float) Texture.Height;
             }
         }
     }
