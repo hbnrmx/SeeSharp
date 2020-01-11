5cabd48ca3dfa49d0ff9046d5ea541a0b9fe1721
Max Hübner <maxshuebner@gmail.com>
2020-01-11 13:20:31 +0100

    use LargeTextureStore

--- SeeSharp.Desktop/Program.cs
+++ SeeSharp.Desktop/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using osu.Framework;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.IO.Stores;
 using osu.Framework.Platform;
 
 namespace SeeSharp.Desktop
@@ -9,11 +11,17 @@ namespace SeeSharp.Desktop
         [STAThread]
         public static void Main()
         {
-            using (GameHost host = Host.GetSuitableHost(@"SeeSharp", false, false))
+            using (GameHost host = Host.GetSuitableHost(@"SeeSharp"))
             using (Game game = new SeeSharp())
             //using (Game game = new SeeSharpTestRunner())
             {
-                host.Dependencies.CacheAs(new SeeSharpStorage(string.Empty, (DesktopGameHost) host));
+                var storage = new SeeSharpStorage(string.Empty, (DesktopGameHost) host);
+                var pageStorage = storage.GetStorageForDirectory("pages");
+                var textureStore = new LargeTextureStore(new TextureLoaderStore(new StorageBackedResourceStore(pageStorage)));
+                
+                host.Dependencies.Cache(storage);
+                host.Dependencies.Cache(textureStore);
+                
                 host.Run(game);
             }
         }
