1b7ed2caaf3a5f83c3a64daba38908450185eb4f
Max Hübner <maxshuebner@gmail.com>
2020-01-31 13:03:18 +0100

    update to net core 3.1, netstandard 2.1

--- .scripts/build.ps1
+++ .scripts/build.ps1
@@ -5,7 +5,7 @@ $buildPath = 'C:\SeeSharp\win10\x64'
 Write-Host "================= BUILDING APP =================" -ForegroundColor Green
 
 cd ../SeeSharp.Desktop
-dotnet publish -o $buildPath -c RELEASE -f netcoreapp3.0 -r win10-x64 /p:PublishSingleFile=true /p:IncludeSymbolsInSingleFile=true
+dotnet publish -o $buildPath -c RELEASE -f netcoreapp3.1 -r win10-x64 /p:PublishSingleFile=true /p:IncludeSymbolsInSingleFile=true
 cd ../.scripts
 
 Write-Host "================== ALL DONE! ===================" -ForegroundColor Green
